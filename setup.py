3ed286fd510856c264c0ab30031e52e0ea95240d
Michael Kerrin <michael.kerrin@gmail.com>
2011-02-22 14:22:34 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "pwt.recipe.closurebuilder",
-    version = "0.9.1",
+    version = "0.9.2",
     author = "Michael Kerrin",
     author_email = "michael.kerrin@gmail.com",
 
